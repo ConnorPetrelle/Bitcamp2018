9983432bd03c8dfe8a07bbe8fd2e11dd590218b6
Jojo Rostock <jojorostock@yahoo.com>
2018-04-07 01:23:29 -0400

    hey

--- Piece.java
+++ Piece.java
@@ -52,71 +52,9 @@ public class Piece {
   }
 
   public int[] get_moves() {
-
+      
   }
 
-<<<<<<< HEAD
-=======
-import java.util.*
-
-
-pulic boolean isValid(int new_x, int new_y)
-{
-  if (this.name.equals("Rook"))
-  {
-    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
-      return false;
-    else if (this.x==new_x && this.y!=new_y)
-      return true;
-    else if (this.x!=new_x && this.y==new_y)
-      return true;
-    else
-      return false;
-  }
-  else if (this.name.equals("Pawn"))
-  {
-    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
-      return false;
-  }
-  else if (this.name.equals("Bishop"))
-  {
-    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
-      return false;
-    for (int i = 0; i == 7; i++)
-    {
-      //need a surrounding check for if piece in the way somehow
-      if(this.x+i == new_x && this.y+i == new_y)
-        return true;
-      if(this.x-i == new_x && this.y+i == new_y)
-        return true;
-      if(this.x-i == new_x && this.y-i == new_y)
-        return true;
-      if(this.x+i == new_x && this.y-i == new_y)
-        return true;
-    }
-    return false
-  }
-  else if (this.name.equals("Queen"))
-  {
-    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
-      return false;
-      else if (this.x==new_x && this.y!=new_y)
-        return true;
-      else if (this.x!=new_x && this.y==new_y)
-        return true;
-      else
-        return false;
-  }
-  else if (this.name.equals("King"))
-  {
-    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
-      return false;
-  }
-  else if (this.name.equals("Knight"))
-  {
-    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
-      return false;
-=======
   public void move(int new_position) {
     if (new_position > 63 || new_position < 0 || !ArrayUtils.contains(this.get_moves(), new_position)) {
       throw new IllegalArgumentException("That is not a valid position")
@@ -124,7 +62,6 @@ pulic boolean isValid(int new_x, int new_y)
     else  {
       this.position = new_position
     }
->>>>>>> 1a09b8b070709b52e73820188da66f13599690e7
   }
 
 
