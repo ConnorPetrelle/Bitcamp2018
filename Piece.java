cfbf4529cb99ed2a7b3701869f1596d2c6be2249
Jojo Rostock <jojorostock@yahoo.com>
2018-04-07 00:08:37 -0400

    added some of isValid, but its not close to done

--- Piece.java
+++ Piece.java
@@ -1,2 +1,51 @@
 import java.util.*
-//hey
+
+
+pulic boolean isValid(int new_x, int new_y)
+{
+  if (this.name.equals("Rook"))
+  {
+    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
+      return false;
+    else if (this.x==new_x && this.y!=new_y)
+      return true;
+    else if (this.x!=new_x && this.y==new_y)
+      return true;
+    else
+      return false;
+  }
+  else if (this.name.equals("Pawn"))
+  {
+    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
+      return false;
+  }
+  else if (this.name.equals("Bishop"))
+  {
+    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
+      return false;
+  }
+  else if (this.name.equals("Queen"))
+  {
+    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
+      return false;
+  }
+  else if (this.name.equals("King"))
+  {
+    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
+      return false;
+  }
+  else if (this.name.equals("Knight"))
+  {
+    if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
+      return false;
+  }
+}
+
+public void move(int new_x, int new_y)
+{
+  if isValid(new_x, new_y)
+  {
+    this.x = new_x;
+    this.y = new_y;
+  }
+}
