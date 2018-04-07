2ebf6243f02d665e7c2b7ea80fa51a6ef9eb2aa5
Jojo Rostock <jojorostock@yahoo.com>
2018-04-07 00:18:52 -0400

    hey

--- Piece.java
+++ Piece.java
@@ -23,11 +23,30 @@ pulic boolean isValid(int new_x, int new_y)
   {
     if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
       return false;
+    for (int i = 0; i == 7; i++)
+    {
+      //need a surrounding check for if piece in the way somehow
+      if(this.x+i == new_x && this.y+i == new_y)
+        return true;
+      if(this.x-i == new_x && this.y+i == new_y)
+        return true;
+      if(this.x-i == new_x && this.y-i == new_y)
+        return true;
+      if(this.x+i == new_x && this.y-i == new_y)
+        return true;
+    }
+    return false
   }
   else if (this.name.equals("Queen"))
   {
     if (new_x < 0 || new_x > 7 || new_y < 0 || new_y > 7)
       return false;
+      else if (this.x==new_x && this.y!=new_y)
+        return true;
+      else if (this.x!=new_x && this.y==new_y)
+        return true;
+      else
+        return false;
   }
   else if (this.name.equals("King"))
   {
