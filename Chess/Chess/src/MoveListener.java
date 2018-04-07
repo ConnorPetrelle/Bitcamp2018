5787ec4406262b7d76372de597f780bc2a8a42e3
Jojo Rostock <jojorostock@yahoo.com>
2018-04-07 19:00:21 -0400

    checking

---
+++ Chess/Chess/src/MoveListener.java
@@ -0,0 +1,41 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MoveListener implements MouseListener{
+
+	Board board;
+	
+	public MoveListener (Board board) {
+		this.board = board;
+	}
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		board.mouseClicked(arg0.getX(), arg0.getY());
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
